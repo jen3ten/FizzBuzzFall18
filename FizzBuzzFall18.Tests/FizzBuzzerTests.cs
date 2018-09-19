3c1714b254c5e4b2829aa242655355b63eefd5e7
Jen E <jen@wecancodeit.org>
2018-09-19 09:03:49 -0400

    initial commit

---
+++ FizzBuzzFall18.Tests/FizzBuzzerTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzFall18.Tests
+{
+    [TestFixture]
+    public class FizzBuzzerTests
+    {
+        //[Test]
+        //public void Foo_Test() 
+        //{
+        //    //Tests that true is equal to false; this is a basic test to make sure that the test process is working
+        //    //Assert is a class of the NUnit Framework
+        //    //the .That() method passes the actual value as the first argument and Is.EqualTo() as the second
+        //    Assert.That(true, Is.EqualTo(false));
+
+        //}
+
+        [Test]
+        public void Get_Returns_1_For_1()
+        {
+            //Create a FizzBuzzer class
+            //Create a FizzBuzzer method
+            //Assert that if you enter a '1' that a '1' is returned
+            FizzBuzzer fizzBuzzer = new FizzBuzzer();
+
+            string result = fizzBuzzer.Get(1);
+
+            Assert.That(result, Is.EqualTo("1"));
+
+        }
+    }
+}
---
+++ FizzBuzzFall18/FizzBuzzer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzFall18
+{
+    public class FizzBuzzer
+    {
+        public string Get(int input)
+        {
+            return "1";
+        }
+    }
+}
